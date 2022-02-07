7ec34e392073d36e779bfc84d1f966698d2eeae6
Fariba Dadko <faribacodes@gmail.com>
2022-02-07 11:50:14 -0800

    adding

---
+++ DOM-Samples/DOM-man/dom.js
@@ -0,0 +1,9 @@
+//1. Replace the area with the pink/orange background with any image you would like. Feel free to find any picture online you would like. You can change the size if you want it, but make sure the picture maintains its proportions.
+let newImg =
+  'https://animalfactguide.com/wp-content/uploads/2021/02/lion_head_male_lion-725x425.jpg';
+
+  //select the elm
+let curImg = document.getElementsByClassName("image")
+
+//
+
