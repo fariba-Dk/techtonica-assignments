9262a406e343128fa9ecfce1f8464bbf8c5febd2
Fariba Dadko <faribacodes@gmail.com>
2022-02-07 22:01:41 -0800

    Object oriented programing exercise

---
+++ OOP/event.js
@@ -0,0 +1,34 @@
+let cc = console.log
+
+class Event {
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+    this.availableTickets = [];
+    //the event availTick property to store ticket types for the event
+  }
+   addAvailableTickets(typeTicket, price);
+}
+
+//Now use this class to create an object and assign values to them.
+//create an event object
+const eventObj1 = new Event('Lady Gaga','Duel Performance HW vampires');
+const eventObj2 = new Event('Skillet', 'Victorious war tour');
+const eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019');
+eventObj1.addAvailableTickets('Floor Ticket', 45)
+eventObj2.addAvailableTickets('Mezzanine Ticket', 75);
+eventObj3.addAvailableTickets('Balcony Ticket', 95);
+
+console.log(eventArray.push(eventObj1, eventObj2, eventObj3))
+
+//create an empty event array
+const eventArray = new Array();
+eventArray.push(eventObj1, eventObj2, eventObj3)
+
+addAvailableTickets
+console.log(eventArray)
+
+let events = document.getElementById('event')
+events.addEventListener('')
+
+
