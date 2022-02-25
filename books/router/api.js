261e04268e46d7606a3aa5303811b2b794b908dd
Fariba Dadko <faribacodes@gmail.com>
2022-02-25 10:56:32 -0800

    route-put

--- books/router/api.js
+++ books/router/api.js
@@ -37,10 +37,10 @@ router.post('/books', (req, res) => {
   } = req.body; //this is our body
 
   const bookExist = bookArr.find((b) => b.isbn === isbn);
-
+  /* MAKE SURE BOOK DOES NOT EXIST*/
   if (bookExist) return res.send('Book Already Exists!!');
 
-  /*if book does not exists then we add newbook with these properties*/
+  /*if book does not exists then we add new book with these properties*/
   const newBook = {
     title,
     isbn,
@@ -56,6 +56,48 @@ router.post('/books', (req, res) => {
   //send this new bok
   bookArr.push(newBook);
   res.send(newBook);
+});
+
+router.put('/books/:id', (req, res) => {
+  const { id } = req.params; //LOOK FOR ID AND destruct book id
+  const {
+    //destruct the body of our request
+    title,
+    isbn,
+    pageCount,
+    publishedDate,
+    thumbnailUrl,
+    shortDescription,
+    longDescription,
+    status,
+    authors,
+    catagories,
+  } = req.params;
+
+  const book = bookArr.find((b) => b.isbn === id);
+  if (!book) res.send('This book does NOT exist!');
+
+  /* we check the value is not defined use the previous value => else updated the value use a terniary operation*/
+  const updatedField = (value, prev) => (!value ? prev : value);
+
+  const updatedBook = {
+    /* we update the fields with pass all properties, default data REM: WE CAN NOT UPDATE ISBN*/
+    ...book,
+    title: updatedField(title, book.title),
+    pageCount: updatedField(pageCount, book.pageCount),
+    publishedDate: updatedField(publishedDate, book.publishedDate),
+    thumbnailUrl: updatedField(thumbnailUrl, book.thumbnailUrl),
+    shortDescription: updatedField(shortDescription, book.shortDescription),
+    longDescription: updatedField(longDescription, book.longDescription),
+    status: updatedField(status, book.status),
+    authors: updatedField(authors, book.authors),
+    catagories: updatedField(catagories, book.catagories),
+  };
+
+  const bookIndex = bookArr.findIndex((b) => b.isbn === id);
+  /* we update using bookIndex and splice() ~~~ splice(start: number, deleteCount?: number):
+   */
+  bookArr.splice(bookIndex, 1, updatedBook);
 });
 
 //router.put('/books/:id', (req, res) => {});
