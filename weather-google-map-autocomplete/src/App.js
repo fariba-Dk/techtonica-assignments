f027c666e3da59f8ef4bffeb9c94c86da61088c0
Fariba Dadko <faribacodes@gmail.com>
2022-03-03 16:53:53 -0800

    YES

--- weather-google-map-autocomplete/src/App.js
+++ weather-google-map-autocomplete/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import Map from './components/Map.js';
+import GoogleMap from './components/GoogleMap.js';
 import Weather from './components/Weather';
-
+import AutoComplete from 'places-autocomplete-react'
 export default function App() {
   return (
     <>
-      <div id='top'>
-        <Map />
-      </div>
 
+      <div id='top'>
+        <GoogleMap />
+      </div>
+      <AutoComplete
+        placesKey="AIzaSyB1fByA0ZCLSYpzyNAlcVJTwIEUNDYuaIE"
+        inputId="address"
+        setAddress={(addressObject) => console.log(addressObject)}
+        required={true}
+        />
       <div id='buttom'>
         <Weather />
       </div>
---
+++ weather-google-map-autocomplete/src/components/GoogleMap.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px',
+};
+
+const center = {
+  lat: 37.773972,
+  lng: -122.431297,
+};
+
+function MyComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'AIzaSyB1fByA0ZCLSYpzyNAlcVJTwIEUNDYuaIE',
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
+
+export default React.memo(MyComponent);
